672820f1a41820526c7f1707bebb8a8856306bcc
v <wavilov@gmail.com>
2020-04-28 19:33:33 +0500

    Disable hardcoded password match PHP check

--- actions/registration.php
+++ actions/registration.php
@@ -21,37 +21,35 @@ if ($fieldsSet == count($user->properties)) {
     }
   }
 
-  if ($_POST['password'] == $_POST['password2']) {
-    $query = 'INSERT INTO ' . $user->table . '(';
-    foreach ($user->properties as $property) {
-      if (!isset($property['isFile'])) {
-        $query .= $property['type'] . ', ';
-      }
+  $query = 'INSERT INTO ' . $user->table . '(';
+  foreach ($user->properties as $property) {
+    if (!isset($property['isFile'])) {
+      $query .= $property['type'] . ', ';
     }
-    $query = substr($query, 0, strlen($query) - 2) . ') VALUES (';
-    foreach ($user->properties as $property) {
-      if (!isset($property['isFile'])) {
-        $query .= '"' . $_POST[$property['type']] . '", ';
-      }
+  }
+  $query = substr($query, 0, strlen($query) - 2) . ') VALUES (';
+  foreach ($user->properties as $property) {
+    if (!isset($property['isFile'])) {
+      $query .= '"' . $_POST[$property['type']] . '", ';
     }
-    $query = substr($query, 0, strlen($query) - 3) . '")';
-    $db->exec($query);
-    if ($db->getError() != '') {
-      $displayError = true;
-    } else {
-      $target_dir = "avatars/";
-      foreach ($user->properties as $property) {
-        if (isset($property['isFile'])) {
-          $target_file = $target_dir . basename($_FILES[$property['type']]['name']);
-          $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-          if ($imageFileType == "jpg" || $imageFileType == "png" || $imageFileType == "jpeg"
-            && $imageFileType == "gif") {
-              $target_file = $target_dir . $db->insertId() . '.' . $imageFileType;
-              move_uploaded_file($_FILES[$property['type']]["tmp_name"], $target_file);
-              session_start();
-              $_SESSION['uid'] = $db->insertId();
-              header('Location: /?mode=in');
-          }
+  }
+  $query = substr($query, 0, strlen($query) - 3) . '")';
+  $db->exec($query);
+  if ($db->getError() != '') {
+    $displayError = true;
+  } else {
+    $target_dir = "avatars/";
+    foreach ($user->properties as $property) {
+      if (isset($property['isFile'])) {
+        $target_file = $target_dir . basename($_FILES[$property['type']]['name']);
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        if ($imageFileType == "jpg" || $imageFileType == "png" || $imageFileType == "jpeg"
+          && $imageFileType == "gif") {
+            $target_file = $target_dir . $db->insertId() . '.' . $imageFileType;
+            move_uploaded_file($_FILES[$property['type']]["tmp_name"], $target_file);
+            session_start();
+            $_SESSION['uid'] = $db->insertId();
+            header('Location: /?mode=in');
         }
       }
     }
